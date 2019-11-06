d859aff0684d63f45fbebbbbe7f04883235332d0
maurogch <maurogch@gmail.com>
2019-11-06 02:35:35 -0300

    Init Backend

---
+++ backend/src/main/java/edu/utn/stockabc/StockAbcApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.stockabc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAbcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/utn/stockabc/StockAbcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.stockabc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

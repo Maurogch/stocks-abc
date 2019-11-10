30765b17cd7a1a498796a3429f335277a671d898
maurogch <maurogch@gmail.com>
2019-11-10 19:41:16 -0300

    delete ckeditor module

--- frontend/src/app/pages/editors/editors-routing.module.ts
+++ frontend/src/app/pages/editors/editors-routing.module.ts
@@ -3,28 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { EditorsComponent } from './editors.component';
 import { TinyMCEComponent } from './tiny-mce/tiny-mce.component';
-import { CKEditorComponent } from './ckeditor/ckeditor.component';
 
-const routes: Routes = [{
-  path: '',
-  component: EditorsComponent,
-  children: [{
-    path: 'tinymce',
-    component: TinyMCEComponent,
-  }, {
-    path: 'ckeditor',
-    component: CKEditorComponent,
-  }],
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: EditorsComponent,
+    children: [
+      {
+        path: 'tinymce',
+        component: TinyMCEComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class EditorsRoutingModule { }
+export class EditorsRoutingModule {}
 
-export const routedComponents = [
-  EditorsComponent,
-  TinyMCEComponent,
-  CKEditorComponent,
-];
+export const routedComponents = [EditorsComponent, TinyMCEComponent];

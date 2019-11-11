305708d43abd6b55e8e4911978ac18083b28e939
maurogch <maurogch@gmail.com>
2019-11-11 05:10:44 -0300

    Home screen and clean navigation sidebar

--- frontend/src/app/pages/pages-menu.ts
+++ frontend/src/app/pages/pages-menu.ts
@@ -2,164 +2,29 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
+    title: 'Home',
+    icon: 'home-outline',
     link: '/pages/dashboard',
     home: true
   },
   {
-    title: 'IoT Dashboard',
-    icon: 'home-outline',
-    link: '/pages/iot-dashboard'
-  },
-  {
-    title: 'FEATURES',
+    title: 'SISTEMA',
     group: true
   },
   {
-    title: 'Layout',
-    icon: 'layout-outline',
-    children: [
-      {
-        title: 'Stepper',
-        link: '/pages/layout/stepper'
-      },
-      {
-        title: 'List',
-        link: '/pages/layout/list'
-      },
-      {
-        title: 'Infinite List',
-        link: '/pages/layout/infinite-list'
-      },
-      {
-        title: 'Accordion',
-        link: '/pages/layout/accordion'
-      },
-      {
-        title: 'Tabs',
-        pathMatch: 'prefix',
-        link: '/pages/layout/tabs'
-      }
-    ]
-  },
-  {
     title: 'Configuraciónes',
+    expanded: true,
     icon: 'edit-2-outline',
     children: [
       {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs'
-      },
-      {
         title: 'Proveedores',
         link: '/pages/forms/layouts'
-      },
-      {
-        title: 'Buttons',
-        link: '/pages/forms/buttons'
-      },
-      {
-        title: 'Datepicker',
-        link: '/pages/forms/datepicker'
-      }
-    ]
-  },
-  {
-    title: 'UI Features',
-    icon: 'keypad-outline',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid'
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons'
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography'
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields'
-      }
-    ]
-  },
-  {
-    title: 'Modal & Overlays',
-    icon: 'browser-outline',
-    children: [
-      {
-        title: 'Dialog',
-        link: '/pages/modal-overlays/dialog'
-      },
-      {
-        title: 'Window',
-        link: '/pages/modal-overlays/window'
-      },
-      {
-        title: 'Popover',
-        link: '/pages/modal-overlays/popover'
-      },
-      {
-        title: 'Toastr',
-        link: '/pages/modal-overlays/toastr'
-      },
-      {
-        title: 'Tooltip',
-        link: '/pages/modal-overlays/tooltip'
-      }
-    ]
-  },
-  {
-    title: 'Extra Components',
-    icon: 'message-circle-outline',
-    children: [
-      {
-        title: 'Calendar',
-        link: '/pages/extra-components/calendar'
-      },
-      {
-        title: 'Progress Bar',
-        link: '/pages/extra-components/progress-bar'
-      },
-      {
-        title: 'Spinner',
-        link: '/pages/extra-components/spinner'
-      },
-      {
-        title: 'Alert',
-        link: '/pages/extra-components/alert'
-      },
-      {
-        title: 'Calendar Kit',
-        link: '/pages/extra-components/calendar-kit'
-      },
-      {
-        title: 'Chat',
-        link: '/pages/extra-components/chat'
-      }
-    ]
-  },
-  {
-    title: 'Gráficos y Calendario',
-    icon: 'pie-chart-outline',
-    children: [
-      {
-        title: 'Clasificación ABC',
-        link: '/pages/charts/echarts'
-      },
-      {
-        title: 'Calendario de entregas',
-        link: '/pages/charts/d3'
       }
     ]
   },
   {
     title: 'Tablas & Datos',
+    expanded: true,
     icon: 'grid-outline',
     children: [
       {
@@ -169,6 +34,21 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Artículos - Políticas de Stock',
         link: '/pages/tables/smart-table'
+      }
+    ]
+  },
+  {
+    title: 'Gráficos y Calendario',
+    expanded: true,
+    icon: 'pie-chart-outline',
+    children: [
+      {
+        title: 'Clasificación ABC',
+        link: '/pages/charts/echarts'
+      },
+      {
+        title: 'Calendario de entregas',
+        link: '/pages/charts/d3'
       }
     ]
   },
